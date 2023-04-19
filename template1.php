cc83e23d2d865a5fe019953a9f56edd575c99a2e
Debopom <dsutradhar201046@bscse.uiu.ac.bd>
2023-04-20 00:53:43 +0600

    2 template done

--- template1.php
+++ template1.php
@@ -1,3 +1,47 @@
+
+<?php
+require 'dbconnect.php';
+$query = "SELECT * FROM form AS f WHERE f.user_id=1";
+$result = $conn->query($query);
+
+
+$row = $result->fetch_assoc();
+$first_name = $row ['fname'];
+$last_name = $row ['lname'];
+$number = $row ['phnnum'];
+$email = $row ['emailad'];
+$address = $row ['address'];
+$nationality = $row ['nationality'];
+$blood = $row ['blood'];
+$gender = $row ['gender'];
+$obj = $row ['Obj'];
+
+$sql_ed = "SELECT * FROM education WHERE user_id = 1 ";
+
+
+$rs = $conn-> query($sql_ed);
+
+$sql_skills = "SELECT * FROM skill WHERE user_id = 1 ";
+
+
+$rs_skill = $conn-> query($sql_skills);
+
+$sql_exp= "SELECT * FROM experience WHERE user_id = 1 ";
+
+
+$rs_exp = $conn-> query($sql_exp);
+
+$sql_honor= "SELECT * FROM honors WHERE user_id = 1 ";
+
+
+$rs_honor = $conn-> query($sql_honor);
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,75 +49,106 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
+<div style = "text-align: center;"><button id="pdfButton" onclick="makepdf()" style = "height: 50px;"><b>Click here to Generate PDF</b></button></div>
 <div id = "main">
     <div id = "boxes">
 
         <div id = "upperbox">
-            <div id= "name">
-                <h3>Debopom</h3>
-                <h1>Sutradhar</h1>
-                <p>Designer</p>
+                <div class = "name">
+                    <p class="fname"><strong><?php echo $first_name ?></strong></p>
+                    <p class ="lname"><strong><?php echo $last_name ?></strong></p>
+                    <p id = "obj"><?php echo $obj ?></p>
+                </div>
 
-            </div>
             
-            <img src="images\template_image.jpg" alt="" style="width:20%;height:200px;float:right;">
+            
+            <img src="images\template_image.jpg" alt="" style="width:200px;height:200px;float:right;">
             
             
         </div>
         <div id = "leftbox">
             <h2>Contact</h2>
-            <h4>address</h4>
-            <h4>email</h4>
-            <h4>number</h4>
+            <h5><?php echo $address ?></h5>
+            <h5><?php echo $email ?></h5>
+            <h5><?php echo $number ?></h5>
             <br><br><br>
             <h2>Education</h2>
             <table id="education">
+            <?php while($data = mysqli_fetch_array($rs)){ ?>
                 <tr>
-                    <td>United International University <br> Bsc <br> CSE</td>
-                    <td width="47%" >2020-Present</td>
+                    <td><?php echo $data['einstitute'] ; ?> <br> <?php echo $data['elevel'] ; ?> <br> <?php echo $data['eresult'] ; ?> </td>
+                    <td width="47%" ><?php echo $data['epassyear'] ; ?></td>
                 </tr>
-                <tr>
-                    <td>United International University <br> Bsc <br> CSE</td>
-                    <td width="47%" >2020-Present</td>
-                </tr>
-                <tr>
-                    <td>United International University <br> Bsc <br> CSE</td>
-                    <td width="47%" >2020-Present</td>
-                </tr>
+            <?php
+            }?>
 
             </table>
-            <h2>References</h2>
+            <h2>Awards</h2>
+            <?php while($data = mysqli_fetch_array($rs_honor)){?>
+            <h4><strong><?php echo $data['hposition'] ; ?></strong></h4> 
+            <p><?php echo $data['hname'] ; ?> <?php echo $data['hplace'] ; ?><br>
+
+            <?php
+            }
+            ?>  
         </div>
         <div id = "rightbox">
-        <h1>Career</h1>
-        <table id="career">
-                <tr>
-                    <td id="job-title">Job-1 <br> 2022 <br> present</td>
-                    <td><h3>company name</h3><h5>position</h5> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis atque ipsam earum sit deserunt quo, officia laborum fugiat fuga voluptatibus labore. Autem facilis odio sequi inventore. Fuga, itaque omnis!</td>
-                </tr>
-                <tr>
-                    <td id="job-title">Job-1 <br> 2022 <br> present</td>
-                    <td><h3>company name</h3><h5>position</h5> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis atque ipsam earum sit deserunt quo, officia laborum fugiat fuga voluptatibus labore. Autem facilis odio sequi inventore. Fuga, itaque omnis!</td>
-                </tr>
-                <tr>
-                    <td id="job-title">Job-1 <br> 2022 <br> present</td>
-                    <td><h3>company name</h3><h5>position</h5> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis atque ipsam earum sit deserunt quo, officia laborum fugiat fuga voluptatibus labore. Autem facilis odio sequi inventore. Fuga, itaque omnis!</td>
-                </tr>
+            <h1>Career</h1>
+            <table id="career">
+                <?php while($data = mysqli_fetch_array($rs_exp)){?>
+                    <tr>
+                        <td id="job-title">Job-1 <br> <?php echo $data['exstart'] ; ?> - <br> <?php echo $data['exend'] ; ?></td>
+                        <td><h3><?php echo $data['excompany'] ; ?></h3><h4><?php echo $data['exdesignation'] ; ?></h4> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis atque ipsam earum sit deserunt quo, officia laborum fugiat fuga voluptatibus labore. Autem facilis odio sequi inventore. Fuga, itaque omnis!</td>
+                    </tr>
+                    <?php
+                }?>
 
-            </table>
-            <h1>Skills</h1>
-            <ul id = "skills">
-                <li>C</li>
-                <li>C++</li>
-                <li>JAVA</li>
-                <li>HTML</li>
-                <li>MS word</li>
-                <li>MS excel</li>
-            </ul>
+                </table>
+                <h1>Skills</h1>
+                <ul id="skills">
+                <?php
+                while($data = mysqli_fetch_array($rs_skill)){?>
+                    <li style="display:inline"><?php echo $data['sname'] ; ?></li>
+                <?php
+                }
+                ?>
+                </ul>
         </div>
     </div>
+    <iframe id="iframe1" style="width : 100% ; border:0;height:0" src="template1.php" frameborder="0"></iframe>
+    
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script src=
+    "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js">
+    </script>
+    <!--JSPDF CDN-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/polyfills.umd.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.0/html2canvas.min.js" integrity="sha512-UcDEnmFoMh0dYHu0wGsf5SKB7z7i5j3GuXHCnb3i4s44hfctoLihr896bxM0zL7jGkcHQXXrJsFIL62ehtd6yQ==" crossorigin="anonymous"
+    referrerpolicy="no-referrer"></script>
 </body>
+
+<script>
+    
+    function makepdf(){
+        var element = document.getElementById('main');
+        html2pdf(element, {
+        margin:       0,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { dpi: 300, letterRendering: true },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        });
+     }
+	    
+          
+        
+
+        
+</script>
 </html>
--- template2.php
+++ template2.php
@@ -1,3 +1,47 @@
+
+<?php
+require 'dbconnect.php';
+$query = "SELECT * FROM form AS f WHERE f.user_id=1";
+$result = $conn->query($query);
+
+
+$row = $result->fetch_assoc();
+$first_name = $row ['fname'];
+$last_name = $row ['lname'];
+$number = $row ['phnnum'];
+$email = $row ['emailad'];
+$address = $row ['address'];
+$nationality = $row ['nationality'];
+$blood = $row ['blood'];
+$gender = $row ['gender'];
+$obj = $row ['Obj'];
+
+$sql_ed = "SELECT * FROM education WHERE user_id = 1 ";
+
+
+$rs = $conn-> query($sql_ed);
+
+$sql_skills = "SELECT * FROM skill WHERE user_id = 1 ";
+
+
+$rs_skill = $conn-> query($sql_skills);
+
+$sql_exp= "SELECT * FROM experience WHERE user_id = 1 ";
+
+
+$rs_exp = $conn-> query($sql_exp);
+
+$sql_honor= "SELECT * FROM honors WHERE user_id = 1 ";
+
+
+$rs_honor = $conn-> query($sql_honor);
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,85 +50,142 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
     <link rel="stylesheet" href="style2.css">
+    
+  
 </head>
 <body>
 <body>
-
-    <div id="page-wrap">
+<div style = "text-align: center;"><button id="pdfButton" onclick="makepdf()" style = "height: 50px;"><b>Click here to Generate PDF</b></button></div>
+    <div id="page-wrap" style="width: 600px;">
     
-        <img src="images\template_image.jpg" id="pic" />
+        <img src="images\template_image.jpg"style="padding-top: 30px;" id="pic" />
     
-        <div id="contact-info" class="vcard">
+        <div id="contact-info" class="vcard" style="padding-top: 30px;">
         
             <!-- Microformats! -->
         
-            <h1 class="fn">Debopom</h1>
+            <h1 class="fn"><?php echo $first_name ?> <?php echo $last_name ?></h1>
         
             <p>
-                Cell: <span class="tel">555-666-7777</span><br />
-                Email: <a class="email" href="">abc@asd.com</a>
+                Cell: <span class="tel"><?php echo $number ?> </span><br />
+                Email: <a class="email" href=""><?php echo $email ?></a>
             </p>
         </div>
                 
         <div id="objective">
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque itaque voluptates, laborum temporibus sit numquam nulla nostrum! Quia velit, id placeat sunt facilis beatae ipsum? Impedit, sapiente! Reiciendis, corrupti nulla.
+            <?php echo $obj ?>
             </p>
         </div>
         
         <div class="clear"></div>
         
-        <dl>
+        <dl style="width: 600px;">
             <dd class="clear"></dd>
             
             <dt>Education</dt>
-            <dd>
-                <h2>United International University</h2>
-                <p><strong>Major:</strong> CSE<br />
+            <dd style="width: 400px;"> <?php
+            while($data = mysqli_fetch_array($rs)){?>
+                <h2><?php echo $data['einstitute'] ; ?></h2>
+                <p><strong>Major/Level:</strong> <?php echo $data['elevel'] ; ?><br />
+                <strong>Result:</strong> <?php echo $data['eresult'] ; ?><br />
+                <strong>passing Year:</strong> <?php echo $data['epassyear'] ; ?><br />
+                </p>
+        <?php
+            }
+        ?>
             </dd>
             
             <dd class="clear"></dd>
             
             <dt>Skills</dt>
-            <dd>
-                <h2>Office skills</h2>
-                <p>Office and records management, database administration, event organization, customer support, travel coordination</p>
-                
-                <h2>Computer skills</h2>
-                <p>C,Java,Python,C++,HTML,CSS,Javascript</p>
+            <dd style="width: 400px;">
+            <ul id="skills">
+            <?php
+            while($data = mysqli_fetch_array($rs_skill)){?>
+                <li style="display:inline"><?php echo $data['sname'] ; ?></li>
+            <?php
+            }
+            ?>
+            </ul> 
             </dd>
             
             <dd class="clear"></dd>
             
             <dt>Experience</dt>
-            <dd>
-                <h2>Job-1<span>Team Leader - Baton Rogue, LA - 1926-2010</span></h2>
+            <dd style="width: 400px;">
+            <?php
+            while($data = mysqli_fetch_array($rs_exp)){?>
+                <h2><?php echo $data['exdesignation'] ; ?><span> <?php echo $data['excompany'] ; ?>, <?php echo $data['exstart'] ; ?>-<?php echo $data['exend'] ; ?></span></h2>
                 <ul>
                     <li>locale_get_primary_language</li>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem deserunt ullam perspiciatis sequi doloremque eos soluta dicta tempora possimus maxime eius adipisci repellendus, velit laboriosam illum cum, sed labore. Excepturi?</li>
                     <li>Provided untimely deaths to all who opposed</li>
                 </ul>
+            <?php
+            }
+            ?>                
                 
             </dd>
             
             <dd class="clear"></dd>
             
-            <dt>Hobbies</dt>
-            <dd>World Domination, Deep Sea Diving, Murder Most Foul</dd>
+            <dt>Honors and Rewards</dt>
+            <dd style="width: 400px;">
+            <?php while($data = mysqli_fetch_array($rs_honor)){?>
+            <h2><strong><?php echo $data['hposition'] ; ?></strong></h2>
+            <p><?php echo $data['hname'] ; ?> <?php echo $data['hplace'] ; ?><br />
+
+            <?php
+            }
+            ?>  
             
-            <dd class="clear"></dd>
+            <!-- <dd class="clear"></dd>
             
             <dt>References</dt>
-            <dd>Available on request</dd>
+            <dd style="width: 400px;">Available on request</dd>
             
-            <dd class="clear"></dd>
+            <dd class="clear"></dd> -->
         </dl>
         
         <div class="clear"></div>
     
     </div>
+    <iframe id="iframe" style="width : 100% ; border:0;height:0" src="template2.php" frameborder="0"></iframe>
+    
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script src=
+    "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js">
+    </script>
+    <!--JSPDF CDN-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/polyfills.umd.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.0/html2canvas.min.js" integrity="sha512-UcDEnmFoMh0dYHu0wGsf5SKB7z7i5j3GuXHCnb3i4s44hfctoLihr896bxM0zL7jGkcHQXXrJsFIL62ehtd6yQ==" crossorigin="anonymous"
+    referrerpolicy="no-referrer"></script>
 
 </body>
+
+<script>
+    
+    
+    function makepdf(){
+        var element = document.getElementById('page-wrap');
+        html2pdf(element, {
+        margin:       0,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { dpi: 192, letterRendering: true },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        });
+     }
+          
+        
+
+        
+</script>
+   
 
 </html>
 </body>
